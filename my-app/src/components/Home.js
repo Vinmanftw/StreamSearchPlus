5c91eb1204b5983c68ba6b600af6434b60b4a090
Chris <Kirch2@gmail.com>
2021-08-24 17:24:00 -0400

    send to vincent

--- my-app/src/components/Home.js
+++ my-app/src/components/Home.js
@@ -1,23 +1,50 @@
-import React from 'react'
-import {NavLink} from 'react-router-dom'
+import React from "react";
+import { NavLink } from "react-router-dom";
 
 function Home(props) {
-    return (
-        <div>
-            <h1>Home</h1>
-            <nav className="Home-NavLinks">
-                <NavLink to="./Netflix"><button id="netflix">Netflix</button></NavLink><br/>
-                <NavLink to="./Hulu"><button id="hulu">Hulu</button></NavLink><br/>            
-                <NavLink to="./Prime"><button id="prime">Amazon Prime Video</button></NavLink><br/>
-                <NavLink to="./Peacock"><button id="peacock">Peacock</button></NavLink><br/>
-                <NavLink to="./Paramount+"><button id="paramount">Paramount+</button></NavLink><br/>
-                <NavLink to="./AppleTV+"><button id="appleTV">AppleTV</button></NavLink><br/>
-                <NavLink to="./Starz"><button id="starz">Starz</button></NavLink><br/>
-                <NavLink to="./Showtime"><button id="showtime">Showtime</button></NavLink><br/>
-                <NavLink to="./HBOMax"><button id="hbomax">HBO Max</button></NavLink><br/>
-            </nav>
-        </div>
-    )
+  return (
+    <div>
+      <h1>Home</h1>
+      <nav className="Home-NavLinks">
+        <NavLink to="./Netflix">
+          <button id="netflix">Netflix</button>
+        </NavLink>
+        <br />
+        <NavLink to="./Hulu">
+          <button id="hulu">Hulu</button>
+        </NavLink>
+        <br />
+        <NavLink to="./Prime">
+          <button id="prime">Prime Video</button>
+        </NavLink>
+        <br />
+        <NavLink to="./Peacock">
+          <button id="peacock">Peacock</button>
+        </NavLink>
+        <br />
+        <NavLink to="./Paramount+">
+          <button id="paramount">Paramount+</button>
+        </NavLink>
+        <br />
+        <NavLink to="./AppleTV+">
+          <button id="appleTV">AppleTV</button>
+        </NavLink>
+        <br />
+        <NavLink to="./Starz">
+          <button id="starz">Starz</button>
+        </NavLink>
+        <br />
+        <NavLink to="./Showtime">
+          <button id="showtime">Showtime</button>
+        </NavLink>
+        <br />
+        <NavLink to="./HBOMax">
+          <button id="hbomax">HBO Max</button>
+        </NavLink>
+        <br />
+      </nav>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
--- my-app/src/components/NavBar.js
+++ my-app/src/components/NavBar.js
@@ -1,24 +1,43 @@
-import React from 'react'
-import { NavLink } from "react-router-dom"
+import React from "react";
+import { NavLink } from "react-router-dom";
 
 function NavBar() {
-    return (
-        <nav>
-            
-            <nav className="NavBar-NavLinks">
-            <NavLink exact to="/"><button id="home">Home</button></NavLink>
-                <NavLink to="./Netflix"><button id="netflix">Netflix</button></NavLink>
-                <NavLink to="./Hulu"><button id="hulu">Hulu</button></NavLink>            
-                <NavLink to="./Prime"><button id="prime">Amazon Prime Video</button></NavLink>
-                <NavLink to="./Peacock"><button id="peacock">Peacock</button></NavLink>
-                <NavLink to="./Paramount+"><button id="paramount">Paramount+</button></NavLink>
-                <NavLink to="./AppleTV+"><button id="appleTV">AppleTV</button></NavLink>
-                <NavLink to="./Starz"><button id="starz">Starz</button></NavLink>
-                <NavLink to="./Showtime"><button id="showtime">Showtime</button></NavLink>
-                <NavLink to="./HBOMax"><button id="hbomax">HBO Max</button></NavLink>
-            </nav>
-        </nav>
-    );
+  return (
+    <nav>
+      <nav className="NavBar-NavLinks">
+        <NavLink exact to="/">
+          <button id="home">Home</button>
+        </NavLink>
+        <NavLink to="./Netflix">
+          <button id="netflix">Netflix</button>
+        </NavLink>
+        <NavLink to="./Hulu">
+          <button id="hulu">Hulu</button>
+        </NavLink>
+        <NavLink to="./Prime">
+          <button id="prime">Prime</button>
+        </NavLink>
+        <NavLink to="./Peacock">
+          <button id="peacock">Peacock</button>
+        </NavLink>
+        <NavLink to="./Paramount+">
+          <button id="paramount">Paramount+</button>
+        </NavLink>
+        <NavLink to="./AppleTV+">
+          <button id="appleTV">AppleTV</button>
+        </NavLink>
+        <NavLink to="./Starz">
+          <button id="starz">Starz</button>
+        </NavLink>
+        <NavLink to="./Showtime">
+          <button id="showtime">Showtime</button>
+        </NavLink>
+        <NavLink to="./HBOMax">
+          <button id="hbomax">HBO Max</button>
+        </NavLink>
+      </nav>
+    </nav>
+  );
 }
 
 export default NavBar;
