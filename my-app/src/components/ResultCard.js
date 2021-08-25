e1c443eb23cd08b4cc48503dedf5341cc6630b6d
Vincent <vao2002@gmail.com>
2021-08-25 15:45:09 -0500

    card IMDb link fix

--- my-app/src/components/ResultCard.js
+++ my-app/src/components/ResultCard.js
@@ -5,18 +5,20 @@ export function ResultCard({ result }) {
     <div id="movieDiv">
       <div className="card">
         <h4>{result.title}</h4>
-        <img
-          className="movie-poster"
-          src={result.posterURLs["500"]}
-          alt={result.title}
-        />
+        
         <a
           href={`https://www.imdb.com/title/${result.imdbID}/`}
           target="_blank"
           rel="noreferrer"
         >
-          imdbID
+        <img
+          className="movie-poster"
+          src={result.posterURLs["500"]}
+          alt={result.title}
+          
+        />
         </a>
+        
       </div>
     </div>
   );
