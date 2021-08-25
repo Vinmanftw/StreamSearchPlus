2690b9d06bde8f6279bdb38bb353a4863a82010e
Chris <Kirch2@gmail.com>
2021-08-25 14:24:08 -0400

    disney file changes

--- my-app/src/components/Disney.js
+++ my-app/src/components/Disney.js
@@ -1,21 +1,37 @@
-import React,{useState} from 'react'
-import { NavLink } from 'react-router-dom'
-import Display from './Display'
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
+import Display from "./Display";
+import { ResultCard } from "./ResultCard";
+import { ResultsFetcher } from "./ResultsFetcher";
+
 function Disney() {
-    const [curType, setCurType] = useState("movies")
-    function handleType(){
-        curType === "movies" ? setCurType("series") : setCurType("movies")
-    }
-    return (
-        <div>
-            <h1>Disney+</h1>
-            <NavLink to="/">Home</NavLink>
-            <div className="types">
-                <button id="typebutton" onClick={handleType}>{curType}</button>
+  const [curType, setCurType] = useState("Movies");
+  function handleType() {
+    curType === "Movies" ? setCurType("Series") : setCurType("Movies");
+  }
+  return (
+    <div>
+      <h1>Disney+</h1>
+      <NavLink to="/">Home</NavLink>
+      <div className="types">
+        <button id="typebutton" onClick={handleType}>
+          {curType}
+        </button>
+      </div>
+      {/* <Display curProvider={"disney"} type={curType} /> */}
+      <ResultsFetcher provider="disney" pg={1} type="movie" keyword={""}>
+        {({ results }) => {
+          return (
+            <div>
+              {results.map((result) => {
+                return <ResultCard result={result} />;
+              })}
             </div>
-            <Display curProvider={"disney"} type={curType}/>
-        </div>
-    )
+          );
+        }}
+      </ResultsFetcher>
+    </div>
+  );
 }
 
-export default Disney
+export default Disney;
