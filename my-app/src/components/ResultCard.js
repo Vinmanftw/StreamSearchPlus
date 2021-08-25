ccade7a1da2151366441e6ef76b4eb73adaf797d
Chris <Kirch2@gmail.com>
2021-08-25 14:21:48 -0400

    added resultcard

---
+++ my-app/src/components/ResultCard.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+export function ResultCard({ result }) {
+  return (
+    <div id="movieDiv">
+      {result.title}
+      <div className="card">
+        <h4>{result.title}</h4>
+        <img
+          className="movie-poster"
+          src={result.posterURLs["500"]}
+          alt={result.title}
+        />
+        <a
+          href={`https://www.imdb.com/title/${result.imdbID}/`}
+          target="_blank"
+          rel="noreferrer"
+        ></a>
+      </div>
+    </div>
+  );
+}
