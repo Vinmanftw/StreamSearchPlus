9371735c9154546c11c087d27454f8d777b380f2
Vincent <vao2002@gmail.com>
2021-08-27 12:39:05 -0500

    react router fixed?????

--- my-app/src/components/NavBar.js
+++ my-app/src/components/NavBar.js
@@ -9,7 +9,7 @@ function NavBar() {
       </NavLink>
       <nav className="NavBar-NavLinks">
         <NavLink to="./netflix">
-          <button id="netflix" onClick={()=>{console.log("yo")}}>
+          <button id="netflix" onClick={()=>{console.log("Never")}}>
             <img
               src="https://www.justwatch.com/images/icon/207360008/s100"
               alt="Netflix"
@@ -17,7 +17,7 @@ function NavBar() {
           </button>
         </NavLink>
         <NavLink to="./Hulu">
-          <button id="hulu">
+          <button id="hulu" onClick={()=>{console.log("gonna")}}>
             <img
               src="https://play-lh.googleusercontent.com/4whGAVjZGrrlNxzheKAfBXrxggtyAb4euWLeQI8fDfVfdnFEZjE0DZTJ8DKoh64pqcIa"
               alt="Hulu"
@@ -25,7 +25,7 @@ function NavBar() {
           </button>
         </NavLink>
         <NavLink to="./Prime">
-          <button id="prime">
+          <button id="prime" onClick={()=>{console.log("give")}}>
             <img
               src="https://www.justwatch.com/images/icon/52449539/s100"
               alt="Prime Video"
@@ -33,7 +33,7 @@ function NavBar() {
           </button>
         </NavLink>
         <NavLink to="./Peacock">
-          <button id="peacock">
+          <button id="peacock" onClick={()=>{console.log("you")}}>
             <img
               src="https://images.justwatch.com/icon/194173870/s100"
               alt="Peacock"
@@ -41,7 +41,7 @@ function NavBar() {
           </button>
         </NavLink>
         <NavLink to="./Paramount+">
-          <button id="paramount">
+          <button id="paramount" onClick={()=>{console.log("up")}}>
             <img
               src="https://images.justwatch.com/icon/242706661/s100"
               alt="Paramount +"
@@ -49,7 +49,7 @@ function NavBar() {
           </button>
         </NavLink>
         <NavLink to="./AppleTV+">
-          <button id="appleTV">
+          <button id="appleTV" onClick={()=>{console.log("never")}}>
             <img
               src="https://www.justwatch.com/images/icon/152862153/s100"
               alt="Apple TV+"
@@ -57,7 +57,7 @@ function NavBar() {
           </button>
         </NavLink>
         <NavLink to="./Starz">
-          <button id="starz">
+          <button id="starz" onClick={()=>{console.log("gonna")}}>
             <img
               src="https://images.justwatch.com/icon/902247/s100"
               alt="Starz"
@@ -65,7 +65,7 @@ function NavBar() {
           </button>
         </NavLink>
         <NavLink to="./Showtime">
-          <button id="showtime">
+          <button id="showtime" onClick={()=>{console.log("let")}}>
             <img
               src="https://images.justwatch.com/icon/677041/s100"
               alt="Showtime"
@@ -73,7 +73,7 @@ function NavBar() {
           </button>
         </NavLink>
         <NavLink to="./HBOMax">
-          <button id="hbomax">
+          <button id="hbomax" onClick={()=>{console.log("you")}}>
             <img
               src="https://play-lh.googleusercontent.com/GmIOtlRHzTffK3WSyNrz4NNrWFh_yUuhQb9UHXztk0ZxeeFzAUD52b9YVTGh7nsdJ8c"
               alt="HBOMAX"
@@ -81,7 +81,7 @@ function NavBar() {
           </button>
         </NavLink>
         <NavLink to="./Disney+">
-          <button id="disney">
+          <button id="disney" onClick={()=>{console.log("down")}}>
             <img
               src="https://www.justwatch.com/images/icon/147638351/s100"
               alt="Disney+"
--- my-app/src/components/ResultsFetcher.js
+++ my-app/src/components/ResultsFetcher.js
@@ -22,7 +22,6 @@ export function ResultsFetcher({ provider, type, pg, keyword, children }) {
     fetch(url({ provider, type, pg, keyword }), FETCH_OPTIONS)
       .then((res) => res.json())
       .then((res) => {
-        console.log(res.results);
         setResults(res.results);
       });
   }, [provider, type, pg, keyword]);
